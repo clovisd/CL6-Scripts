4121b139ee9a03df3d1e82ba16925225358441f9
Clovis <clovisd@users.noreply.github.com>
2018-08-12 21:04:35 -0600

    Added Files

--- setup.sh
+++ setup.sh
@@ -27,6 +27,7 @@ apt --assume-yes -qq update
 apt --assume-yes -qq upgrade
 apt --assume-yes -qq autoremove
 echo -e "${LGREEN}== Done == ${NC}"
+
 #Setup user
 echo -e "${BLUE}<== 2. Users & Passwords ==> ${NC}"
 echo -e "${YELLOW} Setup User: clovisd ${NC}"
@@ -34,6 +35,7 @@ adduser clovisd -q
 echo -e "${YELLOW} Setup User: cl6web ${NC}"
 adduser cl6web -q 
 echo -e "${LGREEN}== Done == ${NC}"
+
 #Setup Bash
 echo -e "${BLUE}<== 3. Setup Bash ==> ${NC}"
 echo -e "${YELLOW} Setting Up Bash for All Users ${NC}"
@@ -42,10 +44,12 @@ cp /home/scripts/setup/.bashrc /home/cl6web/
 if [ ! -d /home/root ]; then mkdir /home/root ; fi
 cp /home/scripts/setup/.bashrc /home/root/
 echo -e "${LGREEN}== Done == ${NC}"
+
 #Setup permissions
 echo -e "${BLUE}<== 4. Setup User Permissions ==> ${NC}"
 /usr/sbin/visudo
 echo -e "${LGREEN}== Done == ${NC}"
+
 #Setup SSH Port
 echo -e "${BLUE}<== 5. Setup SSH Settings ==> ${NC}"
 echo -e "${YELLOW} Setting settings ${NC}"
@@ -54,6 +58,7 @@ nano /etc/ssh/sshd_config
 echo -e "${YELLOW} Restarting SSH Service ${NC}"
 service sshd restart
 echo -e "${LGREEN}== Done == ${NC}"
+
 #Setup Hosts
 echo -e "${BLUE}<== 6. Set Server Name & Hosts ==> ${NC}"
 echo -e "${YELLOW} Set Hostname ${NC}"
@@ -61,6 +66,7 @@ nano /etc/hostname
 echo -e "${YELLOW} Set Hosts ${NC}"
 nano /etc/hosts
 echo -e "${LGREEN}== Done == ${NC}"
+
 #Install Packages
 echo -e "${BLUE}<== 7. Install Apps & Packages ==> ${NC}"
 echo -e "${YELLOW} Setting up CertBot Repo ${NC}"
@@ -80,6 +86,7 @@ service apache2 mysql-server restart | tee -a "$logfile"
 echo -e "${YELLOW} Installing Personal Packages ${NC}"
 apt --assume-yes -qq install mc sl screen htop | tee -a "$logfile"
 echo -e "${LGREEN}== Done == ${NC}"
+
 #SetupPHPAdmin
 echo -e "${BLUE}<== 8. PHPMyAdmin ==> ${NC}"
 apt --assume-yes -qq update
@@ -88,9 +95,28 @@ apt --assume-yes -qq autoremove
 echo -e "${YELLOW} Installing PHPMyAdmin ${NC}"
 apt --assume-yes -qq install phpmyadmin
 echo -e "${LGREEN}== Done == ${NC}"
-#setup universal status files
-#setup host directoies
-#setup apache conf files
+
+#Setup Host Directories
+if [ ! -d /home/cl6web ]; then mkdir /home/cl6web ; fi
+if [ ! -d /home/cl6web/s.cl6.us ]; then mkdir /home/cl6web/s.cl6.us ; fi
+if [ ! -d /home/cl6web/example.com ]; then mkdir /home/cl6web/example.com ; fi
+if [ ! -d /home/cl6web/example.com/logs ]; then mkdir /home/cl6web/example.com/logs ; fi
+if [ ! -d /home/cl6web/example.com/html ]; then mkdir /home/cl6web/example.com/html ; fi
+if [ ! -d /home/cl6web/example.com/backup ]; then mkdir /home/cl6web/example.com/backup ; fi
+if [ ! -d /home/cl6web/example.com/automation ]; then mkdir /home/cl6web/example.com/automation ; fi
+
+#Setup CL6 Greeter Page
+if [ ! -d /home/scripts/setup/greeter ]; then mkdir /home/scripts/setup/greeter ; fi
+cd /home/scripts/setup/greeter
+tar -zxvf /home/scripts/setup/greeter.tar.gz /home/scripts/setup/greeter
+if [ ! -d /home/cl6web/s.cl6.us/greeting ]; then mkdir /home/cl6web/s.cl6.us/greeting ; fi
+
+#Setup Server Status
+if [ ! -d /home/scripts/setup/status ]; then mkdir /home/scripts/setup/status ; fi
+cd /home/scripts/setup/status
+tar -zxvf /home/scripts/setup/status.tar.gz /home/scripts/setup/status
+if [ ! -d /home/cl6web/s.cl6.us/status ]; then mkdir /home/cl6web/s.cl6.us/status ; fi
+
 #setup ssl
 #setup cron ssl
 #certbot renew --dry-run
