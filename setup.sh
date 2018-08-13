f351cb9c224215141512f02c942720fdb13b531d
Clovis <clovisd@users.noreply.github.com>
2018-08-12 20:22:39 -0600

    Push

--- setup.sh
+++ setup.sh
@@ -23,78 +23,78 @@ fi
 
 
 #Setup Updates for New Server
-echo -e "${BLUE}<== 1. Updates & Upgrades ==>"
+echo -e "${BLUE}<== 1. Updates & Upgrades ==> ${NC}"
 sudo apt --assume-yes -qq update >> ${logfile} 2>&1
 sudo apt --assume-yes -qq upgrade >> ${logfile} 2>&1
 sudo apt --assume-yes -qq autoremove >> ${logfile} 2>&1
-echo -e "${LGREEN}== Done =="
+echo -e "${LGREEN}== Done == ${NC}"
 
 #Setup user
-echo -e "${BLUE}<== 2. Users & Passwords ==>"
-echo -e "${YELLOW} Setup User: clovisd"
+echo -e "${BLUE}<== 2. Users & Passwords ==> ${NC}"
+echo -e "${YELLOW} Setup User: clovisd ${NC}"
 adduser clovisd -q
-echo -e "${YELLOW} Setup User: cl6web"
+echo -e "${YELLOW} Setup User: cl6web ${NC}"
 adduser cl6web -q 
-echo -e "${LGREEN}== Done =="
+echo -e "${LGREEN}== Done == ${NC}"
 
 #Setup Bash
-echo -e "${BLUE}<== 3. Setup Bash ==>"
-echo -e "${YELLOW} Setting Up Bash for All Users"
+echo -e "${BLUE}<== 3. Setup Bash ==> ${NC}"
+echo -e "${YELLOW} Setting Up Bash for All Users ${NC}"
 cp /home/scripts/setup/.bashrc /home/clovisd/
 cp /home/scripts/setup/.bashrc /home/cl6web/
 cp /home/scripts/setup/.bashrc /home/root/
-echo -e "${LGREEN}== Done =="
+echo -e "${LGREEN}== Done == ${NC}"
 
 #Setup permissions
-echo -e "${BLUE}<== 4. Setup User Permissions ==>"
+echo -e "${BLUE}<== 4. Setup User Permissions ==> ${NC}"
 sudo /usr/sbin/visudo
-echo -e "${LGREEN}== Done =="
+echo -e "${LGREEN}== Done == ${NC}"
 ​
 #Setup SSH Port
-echo -e "${BLUE}<== 5. Setup SSH Settings ==>"
-echo -e "${YELLOW} Setting settings"
+echo -e "${BLUE}<== 5. Setup SSH Settings ==> ${NC}"
+echo -e "${YELLOW} Setting settings ${NC}"
 sudo nano /etc/ssh/sshd_config
 #sudo vi /etc/ssh/sshd_config
-echo -e "${YELLOW} Restarting SSH Service"
+echo -e "${YELLOW} Restarting SSH Service ${NC}"
 sudo service sshd restart
-echo -e "${LGREEN}== Done =="
+echo -e "${LGREEN}== Done == ${NC}"
 ​
 #Setup Hosts
-echo -e "${BLUE}<== 6. Set Server Name & Hosts ==>"
-echo -e "${YELLOW} Set Hostname"
+echo -e "${BLUE}<== 6. Set Server Name & Hosts ==> ${NC}"
+echo -e "${YELLOW} Set Hostname ${NC}"
 nano /etc/hostname
-echo -e "${YELLOW} Set Hosts"
+echo -e "${YELLOW} Set Hosts ${NC}"
 nano /etc/hosts
-echo -e "${LGREEN}== Done =="
+echo -e "${LGREEN}== Done == ${NC}"
 ​
 #Install Packages
-echo -e "${BLUE}<== 7. Install Apps & Packages ==>"
-echo -e "${YELLOW} Setting up CertBot Repo"
+echo -e "${BLUE}<== 7. Install Apps & Packages ==> ${NC}"
+echo -e "${YELLOW} Setting up CertBot Repo ${NC}"
 sudo add-apt-repository -y ppa:certbot/certbot
-echo -e "${YELLOW} Installing Apache / SQL / CertBot"
+echo -e "${YELLOW} Installing Apache / SQL / CertBot ${NC}"
 sudo apt --assume-yes -qq install apache2 mysql-server python-certbot-apache | tee -a "$logfile"
 sudo apt --assume-yes -qq update >> ${logfile} 2>&1
 sudo apt --assume-yes -qq upgrade >> ${logfile} 2>&1
-echo -e "${YELLOW} Setup SQL Security"
+echo -e "${YELLOW} Setup SQL Security ${NC}"
 sudo mysql_secure_installation --use-default
-echo -e "${YELLOW} Restarting Apache/MySQL"
+echo -e "${YELLOW} Restarting Apache/MySQL ${NC}"
 sudo service apache2 mysql-server restart | tee -a "$logfile"
-echo -e "${YELLOW} Installing PHP Packages"
+echo -e "${YELLOW} Installing PHP Packages ${NC}"
 sudo apt --assume-yes -qq install hp php7.2-mysql php7.2-curl php7.2-xml php7.2-zip  php7.2-gd php7.2-common php7.2-json php7.2-opcache php7.2-readline php7.2-dev php7.2-mbstring php-pear | tee -a "$logfile"
-echo -e "${YELLOW} Restarting Apache/MySQL"
+echo -e "${YELLOW} Restarting Apache/MySQL ${NC}"
 sudo service apache2 mysql-server restart | tee -a "$logfile"
-echo -e "${YELLOW} Installing Personal Packages"
+echo -e "${YELLOW} Installing Personal Packages ${NC}"
 sudo apt --assume-yes -qq install mc sl screen htop | tee -a "$logfile"
-echo -e "${LGREEN}== Done =="
+echo -e "${LGREEN}== Done == ${NC}"
 
 #SetupPHPAdmin
-echo -e "${BLUE}<== 8. PHPMyAdmin ==>"
+echo -e "${BLUE}<== 8. PHPMyAdmin ==> ${NC}"
 sudo apt --assume-yes -qq update >> ${logfile} 2>&1
 sudo apt --assume-yes -qq upgrade >> ${logfile} 2>&1
 sudo apt --assume-yes -qq autoremove >> ${logfile} 2>&1
-echo -e "${YELLOW} Installing PHPMyAdmin"
+echo -e "${YELLOW} Installing PHPMyAdmin ${NC}"
 sudo apt --assume-yes -qq install phpmyadmin
-echo -e "${LGREEN}== Done =="
+echo -e "${LGREEN}== Done == ${NC}"
 
 #setup universal status files
 #setup host directoies
