2995105a29c6b8eebd0fe5c000d5e577703dbb48
Clovis <clovisd@users.noreply.github.com>
2019-01-29 19:29:19 -0700

    Update loader.sh

--- loader.sh
+++ loader.sh
@@ -40,7 +40,7 @@ logfile="/home/scripts/logs/loader.log"
 #Setup Base Programs
 echo -e "${YELLOW} >> Installing Programs"
 DEBIAN_FRONTEND=noninteractive
-apt-get update -qq  & PID=$! >> ${logfile} 2>&1
+apt-get update -qq & PID=$! >> ${logfile} 2>&1
     printf  "${GREEN}[UPDATE:"
 while kill -0 $PID 2> /dev/null; do 
     printf  "."
