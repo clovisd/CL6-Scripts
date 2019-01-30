b62377a1f918ed929a9f79d073a578c964e4e562
Clovis <clovisd@users.noreply.github.com>
2019-01-29 20:52:49 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -69,29 +69,29 @@ echo "Server IP is: ${SERVERIP}"
 
 #Setup Updates for New Server
 echo -e "${BLUE}<== 1. Updates & Upgrades ==> ${NC}"
-apt-get --assume-yes -qq update & PID=$! #>> ${logfile} 2>&1
+apt-get update -qq & PID=$! #>> ${logfile} 2>&1
     printf  "${GREEN}[UPDATE:"
 while kill -0 $PID 2> /dev/null; do 
     printf  "."
     sleep 3
 done
-printf "${GREEN}] - Done"
+printf "${GREEN}] - Done\n"
 echo -e ""
-apt-get --assume-yes -qq upgrade & PID=$! #>> ${logfile} 2>&1
+apt-get upgrade -qq & PID=$! #>> ${logfile} 2>&1
     printf  "${GREEN}[UPGRADE:"
 while kill -0 $PID 2> /dev/null; do 
     printf  "."
     sleep 3
 done
-printf "${GREEN}] - Done"
+printf "${GREEN}] - Done\n"
 echo -e ""
-apt-get --assume-yes -qq autoremove & PID=$! #>> ${logfile} 2>&1
+apt-get autoremove -qq & PID=$! #>> ${logfile} 2>&1
     printf  "${GREEN}[AUTOREMOVE:"
 while kill -0 $PID 2> /dev/null; do 
     printf  "."
     sleep 3
 done
-printf "${GREEN}] - Done"
+printf "${GREEN}] - Done\n"
 echo -e ""
 echo -e "${LGREEN}== Done == ${NC}"
 
