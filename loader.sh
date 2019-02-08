6144aafd1e8f0e15a7db69e6a0bcdb4cdb3f0f98
clovisd <clovisd@users.noreply.github.com>
2019-02-07 18:03:40 -0700

    Update loader.sh

--- loader.sh
+++ loader.sh
@@ -8,7 +8,7 @@
 #bash <(wget -O- -q https://raw.githubusercontent.com/clovisd/CL6-Scripts/master/loader.sh)
 #bash <(wget -O- -q https://goo.gl/yf18Rh)
 
-V='v2.6.1'
+V='v2.7'
 
 #Color Codes
 RED='\033[0;31m'
