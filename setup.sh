83151b6a9a9f3716fe1393165e9b5249f2d5a688
Clovis <clovisd@users.noreply.github.com>
2019-04-26 08:01:05 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -196,7 +196,7 @@ service mysql restart >> ${logfile} 2>&1
 service apache2 restart >> ${logfile} 2>&1
 echo -e "${YELLOW} Installing PHP Packages ${NC}"
 #PHP Base Packages
-(apt-get install -qq php7.2 php7.2-mysql php7.2-curl php7.2-xml php7.2-zip  php7.2-gd php7.2-common php7.2-json php7.2-opcache php7.2-readline php7.2-dev php7.2-gmp php7.2-mbstring php7.2-soap php7.2-xmlrpc php7.2-imap) >> ${logfile} & PID=$! 2>&1
+(apt-get install -qq php7.2 php7.2-mysql php7.2-curl php7.2-xml php7.2-zip php7.2-gd php7.2-common php7.2-json php7.2-opcache php7.2-readline php7.2-dev php7.2-gmp php7.2-mbstring php7.2-soap php7.2-xmlrpc php7.2-imap) >> ${logfile} & PID=$! 2>&1
 #php-pear
     printf  "${GREEN}[INSTALL 1:\n"
 while kill -0 $PID 2> /dev/null; do 
