8388eb7175cc05410c7fc8b19300afb803103e30
Clovis <clovisd@users.noreply.github.com>
2019-08-02 06:35:27 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1013,6 +1013,8 @@ do
           echo -e "${RED} >> Selected $CURRENTINSTALL or ${CURRENTINSTALL}!"
 
 			setupUsers
+			cloudflareInfo
+			uptimerobotInfo
 			basicSetupUtility
 
 			setupBashFiles
